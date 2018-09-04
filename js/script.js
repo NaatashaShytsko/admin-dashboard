d5c1b8ff480add9c1c36468c2d4398b9474a9d94
Natasha Shytsko <natashashitko@gmail.com>
2018-09-04 11:31:01 +0300

    Fixed progress circle

--- js/script.js
+++ js/script.js
@@ -7,14 +7,16 @@ var Circle = function (sel) {
     var circles = document.querySelectorAll(sel);
     [].forEach.call(circles, function (el) {
         var valEl = parseFloat(el.innerHTML);
-        valEl = valEl * 408 / 100;
+
         if ((document.body.clientWidth <= '1200') && (document.body.clientWidth >= '992')) {
-            el.innerHTML = '<figure><svg width="140" height="140"><circle transform="rotate(-90)" r="55" cx="-70" cy="70" /><circle transform="rotate(-90)" style=" stroke-dasharray:' + valEl + 'px 408px;" r="55" cx="-70" cy="70" /><text  x="50%" y="50%" alignment-baseline="middle" text-anchor="middle">' + el.innerHTML + '</text></svg><figcaption></figcaption></figure>';
+            valEl = valEl * 357 / 100;
+            el.innerHTML = '<figure><svg width="140" height="140"><circle transform="rotate(-90)" r="55" cx="-70" cy="70" /><circle transform="rotate(-90)" style=" stroke-dasharray:' + valEl + 'px 357px;" r="55" cx="-70" cy="70" /><text  x="50%" y="50%" alignment-baseline="middle" text-anchor="middle">' + el.innerHTML + '</text></svg><figcaption></figcaption></figure>';
 
         }
         else
+        { valEl = valEl * 408 / 100;
             el.innerHTML = '<figure><svg width="160" height="160"><circle transform="rotate(-90)" r="65" cx="-80" cy="80" /><circle transform="rotate(-90)" style=" stroke-dasharray:' + valEl + 'px 408px;" r="65" cx="-80" cy="80" /><text  x="50%" y="50%" alignment-baseline="middle" text-anchor="middle">' + el.innerHTML + '</text></svg><figcaption></figcaption></figure>';
-
+    }
     });
 };
 Circle('.circle');
